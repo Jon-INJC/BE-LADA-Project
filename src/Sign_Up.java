1dc484feb970ea8df8859e195cd47d7560e6c00a
Jon-INJC <chalachewyonathan8@gmail.com>
2025-11-29 14:52:14 +0300

    Created the sign-up class
    This class will contain the JavaFX GUI for the Sign-up page

---
+++ src/Sign_Up.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Sign_Up extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Sign Up");
+        // Additional setup for the primary stage can be done here
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
