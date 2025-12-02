ca884749773cffacd6e02293c0ecadad155d7de6
Jon-INJC <chalachewyonathan8@gmail.com>
2025-12-02 13:34:15 +0300

    Created the main controller class

---
+++ src/Main_Controller.java
@@ -0,0 +1,44 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+
+public class Main_Controller {
+
+    @FXML
+    private PasswordField Info_CoPass;
+
+    @FXML
+    private TextField Info_Email;
+
+    @FXML
+    private TextField Info_Name;
+
+    @FXML
+    private PasswordField Info_Pass;
+
+    @FXML
+    private TextField Info_UName;
+
+    @FXML
+    private StackPane Left_StackPane;
+
+    @FXML
+    private VBox Right_Vbox;
+
+    @FXML
+    private Button Sign_upBTN;
+
+    @FXML
+    private Label Sign_upLabel;
+
+    @FXML
+    void On_sign_up(ActionEvent event) {
+
+    }
+
+}
