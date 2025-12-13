d686c72054648507840f8cbe99b1441d518fb9eb
gelil-gigi <gigigetinet@gmail.com>
2025-12-13 10:52:43 +0300

    login

---
+++ src/login_Controller.java
@@ -0,0 +1,19 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class login_Controller {
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    void btnLoginisclicked(ActionEvent event) {
+
+    }
+
+}
