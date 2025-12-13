919eb0b48d72250817bf7bd82fc11b379a7a98fc
Jon-INJC <chalachewyonathan8@gmail.com>
2025-12-13 15:43:17 +0300

    Drever's reg

---
+++ src/DriverS_Reg.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class DriverS_Reg extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Sign Up");
+        try{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("DriverS_Reg.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+
+        primaryStage.setResizable(true);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
